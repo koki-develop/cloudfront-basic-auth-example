8068e18f8c05172426222f8eb7c809c7553bf243
koki-develop <kou.pg.0131@gmail.com>
2023-01-13 12:52:20 +0900

    basicauth_username, basicauth_password variable を作成

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,12 @@ variable "name" {
   type    = string
   default = "cf-basic-auth-example"
 }
+
+variable "basicauth_username" {
+  type = string
+}
+
+variable "basicauth_password" {
+  type      = string
+  sensitive = true
+}
